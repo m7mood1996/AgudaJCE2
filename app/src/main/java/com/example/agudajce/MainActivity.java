1e5b9a225b6975bf20342ba9be434bc58504af44
mahmoodnael <m7mood.nael.1996@gmail.com>
2019-06-24 19:32:00 +0300

    hello world

--- app/src/main/java/com/example/agudajce/MainActivity.java
+++ app/src/main/java/com/example/agudajce/MainActivity.java
@@ -287,7 +287,7 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void run() {
 
-                    for(int i =0;i<5;i++){
+                    for(int i =0;i<10;i++){
                         String descri = "";
                         String pic = "";
                         String videourl = "";
